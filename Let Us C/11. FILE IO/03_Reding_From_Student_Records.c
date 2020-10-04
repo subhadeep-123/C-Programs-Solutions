dadb2618fa66215a0c5b2d3a4408128edcbeea76
Subhadeep Banerjee <subhadeep762@gmail.com>
2020-10-05 00:20:58 +0530

    Updated

---
+++ Let Us C/11. FILE IO/03_Reding_From_Student_Records.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <unistd.h>
+struct data
+{
+    char name[30];
+    int age;
+} d;
+int main()
+{
+    FILE *fp;
+    chdir("C:\\Users\\SUBHADEEP\\Desktop");
+    system("cls");
+    fp = fopen("File.txt", "r");
+    if (fp == NULL)
+    {
+        puts("File cannot be open..\n");
+        exit(1);
+    }
+    while (fscanf(fp, "%s %d", &d.name, &d.age) != EOF)
+        printf("%s %d \n", d.name, d.age);
+    printf("\nEnter anything to exit..");
+    getch();
+    return 0;
+}
