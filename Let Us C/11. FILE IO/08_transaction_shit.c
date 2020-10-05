260fa8911e9df94e636763ef7a53850280617cc3
Subhadeep Banerjee <subhadeep762@gmail.com>
2020-10-05 17:26:19 +0530

    Updated

--- Let Us C/11. FILE IO/08_transaction_shit.c
+++ Let Us C/11. FILE IO/08_transaction_shit.c
@@ -1,110 +1,164 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include <conio.h>
-void display(char *);
-void addinfo(int, char *, float);
-void transaction(int, char, float);
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 struct customer
 {
     int accno;
     char name[30];
-    float balc;
+    float balance;
 };
-struct trans
+struct transactions
 {
     int accno;
     char trans_type;
     float amount;
 };
-int main()
+void add_info(int a_num, char *name, float bacl)
 {
-    chdir("C:\\Users\\SUBHADEEP\\Desktop");
-    addinfo(1234, "Subhadeep Banerjee", 10000000);
-    addinfo(2345, "Suvhradip Ghosh", 1000);
-    addinfo(3456, "Shubham Nag", 500);
-    addinfo(4567, "Rajarshee Bhadra", 10000);
-    addinfo(5678, "Soumya Mitra", 10000000);
-    puts("\n\tBefore Transaction");
-    display("customer.dat");
-    transaction(1234, 'd', 200000);
-    puts("\n\tAfter Transaction");
-    display("customer.dat");
-    printf("\nEnter any key.. ");
-    getch();
-    return 0;
+    FILE *fs;
+    struct customer acc_holder;
+    fs = fopen("CUSTOMER.DAT", "rb");
+    if (fs == NULL)
+    {
+        fs = fopen("CUSTOMER.DAT", "wb");
+        puts("CUSTOMER.DAT cannot be opened!");
+        exit(1);
+    }
+    acc_holder.accno = a_num;
+    strcpy(acc_holder.name, name);
+    acc_holder.balance = bacl;
+    fwrite(&acc_holder, sizeof(acc_holder), 1, fs);
+    fclose(fs);
 }
 void display(char *file)
 {
-    FILE *fp;
-    struct customer holder;
-    fp = fopen(file, "rb");
-    while (fread(&holder, sizeof(holder), 1, fp) == 1)
+    FILE *fs;
+    struct customer acc_holder;
+    fs = fopen(file, "rb");
+    if (fs == NULL)
     {
-        printf("\n%d", holder.accno);
-        printf("\t%s", holder.name);
-        printf("\t%f", holder.balc);
+        puts("CUSTOMER.DAT cannot be opened!");
+        exit(1);
     }
-    fclose(fp);
-}
-void addinfo(int accno, char *name, float bal)
-{
-    FILE *fp;
-    struct customer holder;
-    fp = fopen("customer.dat", "rb");
-    if (fp == NULL)
-        fp = fopen("customer.dat", "wb");
-    fseek(fp, 0, SEEK_END);
-    holder.accno = accno;
-    strcpy(holder.name, name);
-    holder.balc = bal;
-    fwrite(&holder, sizeof(holder), 1, fp);
-    fclose(fp);
-}
-
-void transaction(int accno, char ttype, float amount)
-{
-    FILE *fp, *temp;
-    struct customer holder;
-    fp = fopen("customer.dat", "rb");
-    temp = fopen("temp.dat", "wb");
-    while (fread(&holder, sizeof(holder), 1, fp) == 1)
+    while (fread(&acc_holder, sizeof(acc_holder), 1, fs) == 1)
     {
-        if (holder.accno == accno)
+        // printf("%d\n%s\n%f \n", acc_holder.accno, acc_holder.name, acc_holder.balance);
+        printf("\n%d", acc_holder.accno);
+        printf("\n%s", acc_holder.name);
+        printf("\n%f", acc_holder.balance);
+    }
+    fclose(fs);
+}
+void update(int accno, char trans_type, float amount)
+{
+    FILE *fs, *temp;
+    struct customer acc_holder;
+    fs = fopen("CUSTOMER.DAT", "rb");
+    temp = fopen("TEMP.DAT", wb);
+    if (fs == NULL)
+    {
+        puts("CUSTOMER.DAT cannot be opened!");
+        exit(1);
+    }
+    while (fread(&acc_holder, sizeof(acc_holder), 1, fs) == 1)
+    {
+        if (acc_holder.accno == accno)
         {
-            switch (ttype)
+            switch (trans_type)
             {
-            case 'd':
             case 'D':
-                holder.balc += amount;
+            case 'd':
+                acc_holder.balance += amount;
                 break;
-            case 'w':
             case 'W':
-                if ((holder.balc - amount) < 100)
+            case 'w':
+                if ((acc_holder.balance - amount) < 100)
                 {
-                    system("cls");
                     printf("\nYour account balance is low.\n");
                     printf("Transaction failed!!");
-                    _getch();
                 }
                 else
-                    holder.balc -= amount;
+                    acc_holder.balance -= amount;
                 break;
             default:
-                system("cls");
                 puts("Wrong transaction type!!");
                 puts("\nTry Again!!");
                 fclose(fp);
                 fclose(temp);
-                remove("temp.dat");
-                return;
+                remove("TEMP.DAT");
+                break;
             }
         }
-        fwrite(&holder, sizeof(holder), 1, temp);
+        fwrite(&acc_holder, sizeof(acc_holder), 1, temp);
     }
     fclose(fp);
     fclose(temp);
-    remove("customer.dat");
-    rename("temp.dat", "customer.dat");
+    remove("CUSTOMER.DAT");
+    rename("TEMP.DAT", "CUSTOMER.DAT");
+}
+int main()
+{
+    int len, accno, ch;
+    char name[30], trans_type;
+    float balc, amount;
+
+    //Changing the Directory
+    chdir("C:\\Users\\SUBHADEEP\\Desktop");
+
+    system("cls");
+start:
+    while (1)
+    {
+        printf("1. Add Info\n");
+        printf("2. Update\n");
+        printf("3. Display\n");
+        printf("4. Exit\n");
+        printf("5. Clear Screen\n");
+        printf("Enter your choice: ");
+        fflush(stdin);
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            printf("How many records do you want to enter: ");
+            scanf("%d", &len);
+            for (int i = 0; i < len; i++)
+            {
+                puts("\nEnter Account Number: ");
+                scanf("%d", &accno);
+                fflush(stdin);
+                puts("Enter Name: ");
+                gets(name);
+                puts("Enter Account Balance: ");
+                scanf("%f", &balc);
+                add_info(accno, name, balc);
+            }
+            break;
+        case 2:
+            puts("Enter Account No: ");
+            scanf("%d", &accno);
+            puts("Enter Transaction Type D = Deposit/ W = Withdrawl: ");
+            trans_type = getche();
+            puts("Enter Anount: ");
+            scanf("%f", &amount);
+            update(accno, trans_type, amount);
+            break;
+        case 3:
+            display("CUSTOMER.DAT");
+            break;
+        case 4:
+            exit(0);
+        case 5:
+            system("cls");
+            goto start;
+        default:
+            puts("\nEner a Valid Choice");
+            break;
+        }
+    }
+    printf("\nEnter anything to exit: ");
+    getch();
+    return 0;
 }
