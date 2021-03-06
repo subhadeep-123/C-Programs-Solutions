e0ae55f776a1a122459b61fcfaff7acf1ae49d23
Subhadeep Banerjee <subhadeep762@gmail.com>
2021-03-07 02:36:37 +0530

    Updated

--- Let Us C/02. C Instructions/7. Wind Chill Factor.c
+++ Let Us C/02. C Instructions/7. Wind Chill Factor.c
@@ -1,14 +1,14 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 int main()
 {
 	double wcf, temp, wc;
 	printf("Enter The Temperature in degrees:-\n");
-	scanf("%lf",&temp);
+	scanf("%lf", &temp);
 	printf("Enter The Wind Velocity:-\n");
-	scanf("%lf",&wc);
-	
-	wcf = 35.74 + (0.6215 * temp)+ ((0.4275 * temp)- 35.75) * pow(wc,0.16);
-	printf("The Wind Chill Factor is :- %lf",wcf);
+	scanf("%lf", &wc);
+
+	wcf = 35.74 + (0.6215 * temp) + ((0.4275 * temp) - 35.75) * pow(wc, 0.16);
+	printf("The Wind Chill Factor is :- %lf", wcf);
 	return 0;
 }
